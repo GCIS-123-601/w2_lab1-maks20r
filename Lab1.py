009f75260c7ffd47430f6c84ef36368903eb92eb
maks20r <90239298+maks20r@users.noreply.github.com>
2021-09-21 11:16:05 +0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,30 @@
+import turtle
+def turtle_state():
+    turtle.isdown()
+    turtle.heading()
+    turtle.xcor()
+    turtle.ycor()
+    turtle.done()
+
+def main():
+    turtle_state()
+
+main()
+
+
+
+
+
+import turtle
+turtle.forward(100)
+turtle.left(87)
+turtle.setheading(127)
+turtle.up()
+turtle.down()
+turtle.goto(50, 50)
+turtle.home()
+turtle.circle(10)
+turtle.done()
+
+
+main()
